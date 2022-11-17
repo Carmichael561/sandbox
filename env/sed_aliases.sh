c0b4934c343bcc21e19531f1e7c9165f7157caf0
Carmichael561 <gomonovych@gmail.com>
2022-11-17 16:50:40 +0100

    Update sed_aliases.sh

--- env/sed_aliases.sh
+++ env/sed_aliases.sh
@@ -1,2 +1,5 @@
  
 alias enum="sed 's/[a-z]/\U&/g' | sed 's/ /_/g'"
+
+# sed -e "s/[[:space:]]\+/ /g"            - replace all tabs and spaces with a single space
+# sed 's/\([^ ]*\) *\(.*\)/\2 \1/'        - sed move first (\1) column to end (\2)
